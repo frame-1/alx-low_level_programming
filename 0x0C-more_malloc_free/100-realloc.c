d1356408525b66d1d0e341b07b53c9eb8e601926
frame-1 <muthee031@gmail.com>
2022-04-07 11:09:10 -0700

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,30 +9,29 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, *realloc;
-	unsigned int i;
+	char *new_location, *new_ptr;
+	unsigned int i = 0;
 
-	if (ptr != NULL)
-		clone = ptr;
-	else
-	{
-	       	return (malloc(new_size));
-       	}
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0 && ptr != NULL)
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
-		return (0);
+		return (NULL);
 	}
-	relloc = malloc(new_size);
-
-	if (relloc == NULL)
-		return (0);
-	for (i = 0; i < (old_size || i < new_size); i++)
-	{
-		*(relloc + i) = clone[i];
-	}
+	new_location = malloc(new_size);
+	new_ptr = ptr;
+	if (old_size > new_size)
+		old_size = new_size;
+	for (i = 0; i < old_size; i++)
+		new_location[i] = new_ptr[i];
 	free(ptr);
-return (relloc);
+	return (new_location);
 }
