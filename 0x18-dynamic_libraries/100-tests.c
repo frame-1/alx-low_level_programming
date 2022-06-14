fde7b9d5158370792c969b01a83a76cabddbd46b
frame-1 <muthee031@gmail.com>
2022-06-14 05:34:32 -0700

    Tasks

---
+++ 0x18-dynamic_libraries/100-tests.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - function that adds
+ * @a: first input integer
+ * @b: second input integer
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - fuction that subtracts
+ * @a: first input integer
+ * @b: second input integer
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function that multiplies
+ * @a: first input integer
+ * @b: second input integer
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function that divides
+ * @a: first input integer
+ * @b: second input integer
+ * Return: remainder
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function that modulo divides
+ * @a: first input integer
+ * @b: second input integer
+ * Return: equivalence class
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
